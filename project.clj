563e77f8336b12a45ee9166eb7c6a7a83d0abee6
Michael Sappler <michaelsappler@gmail.com>
2025-09-06 15:18:32 +0530

    Fix project.clj

--- project.clj
+++ project.clj
@@ -2,5 +2,5 @@
   :repositories [["jitpack" "https://jitpack.io"]]
   :dependencies [[org.clojure/clojure "1.12.0"]
                  [clojure.gdx "1.13.5"]
-                 [com.badlogicgames.gdx/gdx-freetype          ~libgdx-version]
-                 [com.badlogicgames.gdx/gdx-freetype-platform ~libgdx-version :classifier "natives-desktop"]])
+                 [com.badlogicgames.gdx/gdx-freetype          "1.13.5"]
+                 [com.badlogicgames.gdx/gdx-freetype-platform "1.13.5" :classifier "natives-desktop"]])
